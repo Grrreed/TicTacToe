3b0d96351d3bf6f0c8901a096835d27c97582c26
Bryan Bailon <74363829+Grrreed@users.noreply.github.com>
2021-08-02 16:13:40 +0800

    Add files via upload

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Tictactoetbl
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ticpan1 = new System.Windows.Forms.Panel();
+            this.btontic9 = new System.Windows.Forms.Button();
+            this.btontic6 = new System.Windows.Forms.Button();
+            this.btontic3 = new System.Windows.Forms.Button();
+            this.btontic8 = new System.Windows.Forms.Button();
+            this.btontic5 = new System.Windows.Forms.Button();
+            this.btontic2 = new System.Windows.Forms.Button();
+            this.btontic7 = new System.Windows.Forms.Button();
+            this.btontic4 = new System.Windows.Forms.Button();
+            this.btontic1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblscoreo = new System.Windows.Forms.Label();
+            this.lblscorex = new System.Windows.Forms.Label();
+            this.lblo = new System.Windows.Forms.Label();
+            this.lblx = new System.Windows.Forms.Label();
+            this.btnNewgame = new System.Windows.Forms.Button();
+            this.Ticpan1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Ticpan1
+            // 
+            this.Ticpan1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Ticpan1.Controls.Add(this.btontic9);
+            this.Ticpan1.Controls.Add(this.btontic6);
+            this.Ticpan1.Controls.Add(this.btontic3);
+            this.Ticpan1.Controls.Add(this.btontic8);
+            this.Ticpan1.Controls.Add(this.btontic5);
+            this.Ticpan1.Controls.Add(this.btontic2);
+            this.Ticpan1.Controls.Add(this.btontic7);
+            this.Ticpan1.Controls.Add(this.btontic4);
+            this.Ticpan1.Controls.Add(this.btontic1);
+            this.Ticpan1.Location = new System.Drawing.Point(13, 13);
+            this.Ticpan1.Name = "Ticpan1";
+            this.Ticpan1.Size = new System.Drawing.Size(608, 412);
+            this.Ticpan1.TabIndex = 0;
+            // 
+            // btontic9
+            // 
+            this.btontic9.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btontic9.Location = new System.Drawing.Point(417, 274);
+            this.btontic9.Name = "btontic9";
+            this.btontic9.Size = new System.Drawing.Size(147, 126);
+            this.btontic9.TabIndex = 8;
+            this.btontic9.UseVisualStyleBackColor = true;
+            this.btontic9.Click += new System.EventHandler(this.btontic9_Click);
+            // 
+            // btontic6
+            // 
+            this.btontic6.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btontic6.Location = new System.Drawing.Point(417, 142);
+            this.btontic6.Name = "btontic6";
+            this.btontic6.Size = new System.Drawing.Size(147, 126);
+            this.btontic6.TabIndex = 7;
+            this.btontic6.UseVisualStyleBackColor = true;
+            this.btontic6.Click += new System.EventHandler(this.Btntic6_Click);
+            // 
+            // btontic3
+            // 
+            this.btontic3.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btontic3.Location = new System.Drawing.Point(417, 10);
+            this.btontic3.Name = "btontic3";
+            this.btontic3.Size = new System.Drawing.Size(147, 126);
+            this.btontic3.TabIndex = 6;
+            this.btontic3.UseVisualStyleBackColor = true;
+            this.btontic3.Click += new System.EventHandler(this.btontic3_Click);
+            // 
+            // btontic8
+            // 
+            this.btontic8.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btontic8.Location = new System.Drawing.Point(219, 274);
+            this.btontic8.Name = "btontic8";
+            this.btontic8.Size = new System.Drawing.Size(147, 126);
+            this.btontic8.TabIndex = 5;
+            this.btontic8.UseVisualStyleBackColor = true;
+            this.btontic8.Click += new System.EventHandler(this.btontic8_Click);
+            // 
+            // btontic5
+            // 
+            this.btontic5.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btontic5.Location = new System.Drawing.Point(219, 142);
+            this.btontic5.Name = "btontic5";
+            this.btontic5.Size = new System.Drawing.Size(147, 126);
+            this.btontic5.TabIndex = 4;
+            this.btontic5.UseVisualStyleBackColor = true;
+            this.btontic5.Click += new System.EventHandler(this.btontic5_Click);
+            // 
+            // btontic2
+            // 
+            this.btontic2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btontic2.Location = new System.Drawing.Point(219, 10);
+            this.btontic2.Name = "btontic2";
+            this.btontic2.Size = new System.Drawing.Size(147, 126);
+            this.btontic2.TabIndex = 3;
+            this.btontic2.UseVisualStyleBackColor = true;
+            this.btontic2.Click += new System.EventHandler(this.btontic2_Click);
+            // 
+            // btontic7
+            // 
+            this.btontic7.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btontic7.Location = new System.Drawing.Point(23, 274);
+            this.btontic7.Name = "btontic7";
+            this.btontic7.Size = new System.Drawing.Size(147, 126);
+            this.btontic7.TabIndex = 2;
+            this.btontic7.UseVisualStyleBackColor = true;
+            this.btontic7.Click += new System.EventHandler(this.btontic7_Click);
+            // 
+            // btontic4
+            // 
+            this.btontic4.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btontic4.Location = new System.Drawing.Point(23, 142);
+            this.btontic4.Name = "btontic4";
+            this.btontic4.Size = new System.Drawing.Size(147, 126);
+            this.btontic4.TabIndex = 1;
+            this.btontic4.UseVisualStyleBackColor = true;
+            this.btontic4.Click += new System.EventHandler(this.btontic4_Click);
+            // 
+            // btontic1
+            // 
+            this.btontic1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btontic1.Location = new System.Drawing.Point(23, 10);
+            this.btontic1.Name = "btontic1";
+            this.btontic1.Size = new System.Drawing.Size(147, 126);
+            this.btontic1.TabIndex = 0;
+            this.btontic1.Text = " ";
+            this.btontic1.UseVisualStyleBackColor = true;
+            this.btontic1.Click += new System.EventHandler(this.btontic1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.lblscoreo);
+            this.panel1.Controls.Add(this.lblscorex);
+            this.panel1.Controls.Add(this.lblo);
+            this.panel1.Controls.Add(this.lblx);
+            this.panel1.Location = new System.Drawing.Point(627, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(234, 110);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblscoreo
+            // 
+            this.lblscoreo.AutoSize = true;
+            this.lblscoreo.BackColor = System.Drawing.Color.White;
+            this.lblscoreo.Location = new System.Drawing.Point(116, 69);
+            this.lblscoreo.MinimumSize = new System.Drawing.Size(5, 5);
+            this.lblscoreo.Name = "lblscoreo";
+            this.lblscoreo.Size = new System.Drawing.Size(7, 15);
+            this.lblscoreo.TabIndex = 3;
+            this.lblscoreo.Text = "\r\n";
+            // 
+            // lblscorex
+            // 
+            this.lblscorex.AutoSize = true;
+            this.lblscorex.BackColor = System.Drawing.Color.White;
+            this.lblscorex.Location = new System.Drawing.Point(116, 19);
+            this.lblscorex.MinimumSize = new System.Drawing.Size(5, 5);
+            this.lblscorex.Name = "lblscorex";
+            this.lblscorex.Size = new System.Drawing.Size(5, 15);
+            this.lblscorex.TabIndex = 3;
+            // 
+            // lblo
+            // 
+            this.lblo.AutoSize = true;
+            this.lblo.Location = new System.Drawing.Point(14, 70);
+            this.lblo.Name = "lblo";
+            this.lblo.Size = new System.Drawing.Size(85, 15);
+            this.lblo.TabIndex = 1;
+            this.lblo.Text = "Player O score:";
+            // 
+            // lblx
+            // 
+            this.lblx.AutoSize = true;
+            this.lblx.Location = new System.Drawing.Point(14, 19);
+            this.lblx.Name = "lblx";
+            this.lblx.Size = new System.Drawing.Size(83, 15);
+            this.lblx.TabIndex = 0;
+            this.lblx.Text = "Player x Score:";
+            // 
+            // btnNewgame
+            // 
+            this.btnNewgame.Location = new System.Drawing.Point(679, 139);
+            this.btnNewgame.Name = "btnNewgame";
+            this.btnNewgame.Size = new System.Drawing.Size(147, 55);
+            this.btnNewgame.TabIndex = 9;
+            this.btnNewgame.Text = "New Game";
+            this.btnNewgame.UseVisualStyleBackColor = true;
+            this.btnNewgame.Click += new System.EventHandler(this.btnNewgame_Click);
+            // 
+            // Tictactoetbl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(873, 442);
+            this.Controls.Add(this.btnNewgame);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Ticpan1);
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.Name = "Tictactoetbl";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Ticpan1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Newgame;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Panel Ticpan1;
+        private System.Windows.Forms.Button btntic9;
+        private System.Windows.Forms.Button btontic6;
+        private System.Windows.Forms.Button btontic3;
+        private System.Windows.Forms.Button btontic8;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button btontic2;
+        private System.Windows.Forms.Button btontic7;
+        private System.Windows.Forms.Button btontic4;
+        private System.Windows.Forms.Button btontic1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblscoreo;
+        private System.Windows.Forms.Label lblscorex;
+        private System.Windows.Forms.Label lblo;
+        private System.Windows.Forms.Label lblx;
+        private System.Windows.Forms.Button btnNewgame;
+        private System.Windows.Forms.Button btontic5;
+        private System.Windows.Forms.Button btntic1;
+        private System.Windows.Forms.Button btontic9;
+    }
+}
+
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Tictactoetbl : Form
+    {
+        Boolean checker;
+        int plusone;
+        
+        void Enable_false()
+        {
+            btontic1.Enabled = false;
+            btontic2.Enabled = false;
+            btontic3.Enabled = false;
+            btontic4.Enabled = false;
+            btontic5.Enabled = false;
+            btontic6.Enabled = false;
+            btontic7.Enabled = false;
+            btontic8.Enabled = false;
+            btontic9.Enabled = false;
+        }
+        void score()
+        {
+            if (btontic1.Text == "X" && btontic4.Text == "X" && btontic7.Text == "X")
+            {
+                MessageBox.Show("The winner is Player X !", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                plusone = int.Parse(lblscorex.Text);
+                lblscorex.Text = Convert.ToString(plusone);
+                Enable_false();
+            }
+            if (btontic1.Text == "X" && btontic2.Text == "X" && btontic3.Text == "X")
+            {
+                MessageBox.Show("The winner is Player X !", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                plusone = int.Parse(lblscorex.Text);
+                lblscorex.Text = Convert.ToString(plusone);
+                Enable_false();
+            }
+            if (btontic1.Text == "X" && btontic5.Text == "X" && btontic9.Text == "X")
+            {
+                MessageBox.Show("The winner is Player X !", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                plusone = int.Parse(lblscorex.Text);
+                lblscorex.Text = Convert.ToString(plusone);
+                Enable_false();
+            }
+            if (btontic2.Text == "X" && btontic5.Text == "X" && btontic8.Text == "X")
+            {
+                MessageBox.Show("The winner is Player X !", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                plusone = int.Parse(lblscorex.Text);
+                lblscorex.Text = Convert.ToString(plusone);
+                Enable_false();
+            }
+            if (btontic4.Text == "X" && btontic5.Text == "X" && btontic6.Text == "X")
+            {
+                MessageBox.Show("The winner is Player X !", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                plusone = int.Parse(lblscorex.Text);
+                lblscorex.Text = Convert.ToString(plusone);
+                Enable_false();
+            }
+            if (btontic7.Text == "X" && btontic8.Text == "X" && btontic9.Text == "X")
+            {
+                MessageBox.Show("The winner is Player X !", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                plusone = int.Parse(lblscorex.Text);
+                lblscorex.Text = Convert.ToString(plusone);
+                Enable_false();
+            }
+            if (btontic3.Text == "X" && btontic6.Text == "X" && btontic9.Text == "X")
+            {
+                MessageBox.Show("The winner is Player X !", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                plusone = int.Parse(lblscorex.Text);
+                lblscorex.Text = Convert.ToString(plusone);
+                Enable_false();
+            }
+            if (btontic3.Text == "X" && btontic5.Text == "X" && btontic7.Text == "X")
+            {
+                MessageBox.Show("The winner is Player X !", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                plusone = int.Parse(lblscorex.Text);
+                lblscorex.Text = Convert.ToString(plusone);
+                Enable_false();
+            }
+            if (btontic1.Text == "O" && btontic4.Text == "O" && btontic7.Text == "O")
+            {
+                MessageBox.Show("The winner is Player O !", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                plusone = int.Parse(lblscoreo.Text);
+                lblscoreo.Text = Convert.ToString(plusone);
+                Enable_false();
+            }
+            if (btontic1.Text == "O" && btontic2.Text == "O" && btontic3.Text == "O")
+            {
+                MessageBox.Show("The winner is Player O !", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                plusone = int.Parse(lblscoreo.Text);
+                lblscoreo.Text = Convert.ToString(plusone);
+                Enable_false();
+            }
+            if (btontic1.Text == "O" && btontic5.Text == "O" && btontic9.Text == "O")
+            {
+                MessageBox.Show("The winner is Player O !", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                plusone = int.Parse(lblscoreo.Text);
+                lblscoreo.Text = Convert.ToString(plusone);
+                Enable_false();
+            }
+            if (btontic2.Text == "O" && btontic5.Text == "O" && btontic8.Text == "O")
+            {
+                MessageBox.Show("The winner is Player O !", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                plusone = int.Parse(lblscoreo.Text);
+                lblscoreo.Text = Convert.ToString(plusone);
+                Enable_false();
+            }
+            if (btontic4.Text == "O" && btontic5.Text == "O" && btontic6.Text == "O")
+            {
+                MessageBox.Show("The winner is Player O !", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                plusone = int.Parse(lblscoreo.Text);
+                lblscoreo.Text = Convert.ToString(plusone);
+                Enable_false();
+            }
+            if (btontic7.Text == "O" && btontic8.Text == "O" && btontic9.Text == "O")
+            {
+                MessageBox.Show("The winner is Player O !", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                plusone = int.Parse(lblscoreo.Text);
+                lblscoreo.Text = Convert.ToString(plusone);
+                Enable_false();
+            }
+            if (btontic3.Text == "O" && btontic6.Text == "O" && btontic9.Text == "O")
+            {
+                MessageBox.Show("The winner is Player O !", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                plusone = int.Parse(lblscoreo.Text);
+                lblscoreo.Text = Convert.ToString(plusone);
+                Enable_false();
+            }
+            if (btontic3.Text == "O" && btontic5.Text == "O" && btontic7.Text == "O")
+            {
+                MessageBox.Show("The winner is Player O !", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                plusone = int.Parse(lblscoreo.Text);
+                lblscoreo.Text = Convert.ToString(plusone);
+                Enable_false();
+            }
+        }
+        private void enablefalse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tictactoetbl()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Btntic6_Click(object sender, EventArgs e)
+        {
+            if (checker == false)
+            {
+                btontic6.Text = "X";
+                checker = true;
+            }
+            else
+            {
+                btontic6.Text = "O";
+                checker = false;
+            }
+            score();
+            btontic6.Enabled = false; 
+        }
+        private void btontic1_Click(object sender, EventArgs e)
+        {
+            if (checker == false)
+            {
+                btontic1.Text = "X";
+                checker = true;
+            }
+            else
+            {
+                btontic1.Text = "O";
+                checker = false;
+            }
+            score();
+            btontic1.Enabled = false;
+        }
+        private void btontic2_Click(object sender, EventArgs e)
+        {
+            if (checker == false)
+            {
+                btontic2.Text = "X";
+                checker = true;
+            }
+            else
+            {
+                btontic2.Text = "O";
+                checker = false;
+            }
+            score();
+            btontic2.Enabled = false;
+        }
+        private void btontic3_Click(object sender, EventArgs e)
+        {
+            if (checker == false)
+            {
+                btontic3.Text = "X";
+                checker = true;
+            }
+            else
+            {
+                btontic3.Text = "O";
+                checker = false;
+            }
+            score();
+            btontic3.Enabled = false;
+        }
+        private void btontic4_Click(object sender, EventArgs e)
+        {
+            if (checker == false)
+            {
+                btontic4.Text = "X";
+                checker = true;
+            }
+            else
+            {
+                btontic4.Text = "O";
+                checker = false;
+            }
+            score();
+            btontic4.Enabled = false;
+        }
+        private void btontic5_Click(object sender, EventArgs e)
+        {
+            if (checker == false)
+            {
+                btontic5.Text = "X";
+                checker = true;
+            }
+            else
+            {
+                btontic5.Text = "O";
+                checker = false;
+            }
+            score();
+            btontic5.Enabled = false;
+        }
+        private void btontic7_Click(object sender, EventArgs e)
+        {
+            if (checker == false)
+            {
+                btontic7.Text = "X";
+                checker = true;
+            }
+            else
+            {
+                btontic7.Text = "O";
+                checker = false;
+            }
+            score();
+            btontic7.Enabled = false;
+        }
+        private void btontic8_Click(object sender, EventArgs e)
+        {
+            if (checker == false)
+            {
+                btontic8.Text = "X";
+                checker = true;
+            }
+            else
+            {
+                btontic8.Text = "O";
+                checker = false;
+            }
+            score();
+            btontic8.Enabled = false;
+        }
+        private void btontic9_Click(object sender, EventArgs e)
+        {
+            if (checker == false)
+            {
+                btontic9.Text = "X";
+                checker = true;
+            }
+            else
+            {
+                btontic9.Text = "O";
+                checker = false;
+            }
+            score();
+            btontic9.Enabled = false;
+        }
+        private void btnNewgame_Click(object sender, EventArgs e)
+        {
+            btontic1.Enabled = true;
+            btontic2.Enabled = true;
+            btontic3.Enabled = true;
+            btontic4.Enabled = true;
+            btontic5.Enabled = true;
+            btontic6.Enabled = true;
+            btontic7.Enabled = true;
+            btontic8.Enabled = true;
+            btontic9.Enabled = true;
+
+            btontic1.Text = "";
+            btontic2.Text = "";
+            btontic3.Text = "";
+            btontic4.Text = "";
+            btontic5.Text = "";
+            btontic6.Text = "";
+            btontic7.Text = "";
+            btontic8.Text = "";
+            btontic9.Text = "";
+        }
+    }
+}
